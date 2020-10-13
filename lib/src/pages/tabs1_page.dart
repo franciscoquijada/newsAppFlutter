50f41680989a58175a6b2ffe71c4732a8579272b
franciscoquijada <franskferdinand@hotmail.com>
2020-10-12 23:20:02 -0300

    correccion en mixin

--- lib/src/pages/tabs1_page.dart
+++ lib/src/pages/tabs1_page.dart
@@ -8,7 +8,8 @@ class Tabs1Page extends StatefulWidget {
   _Tabs1PageState createState() => _Tabs1PageState();
 }
 
-class _Tabs1PageState extends State<Tabs1Page> {
+class _Tabs1PageState extends State<Tabs1Page>
+    with AutomaticKeepAliveClientMixin {
   @override
   Widget build(BuildContext context) {
     final headlines = Provider.of<NewsService>(context).headlines;
